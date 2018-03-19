0d4598f51577de5f89d968a4de7bfc9710aab171
user <user@MSI>
2018-03-19 14:13:54 -0500

    studio 8

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,32 @@
 package studio8;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 // TODO: Develop an algorithm to count steps in accelerometer data
 //    Major steeps:
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+ public class CountSteps{
+	 
+	 public CountSteps() {
+		 
+	 }
+	 
+	 public static void main(String[] args) {
+		 try {
+			Scanner sys = new Scanner(new File("data/wdata.csv"));
+			while(sys.hasNext()) {
+				System.out.println(sys.next());
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+	 }
+	 
+	 
+ }
---
+++ steps/steps.ino
@@ -0,0 +1,26 @@
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h>
+
+MMA8452Q accel;
+
+void setup() {
+  Serial.begin(9600);
+  accel.init(SCALE_4G,ODR_6);
+}
+
+void loop() {
+  if(accel.available()){
+    accel.read();
+    writeAccels();
+  }
+}
+
+void writeAccels(){
+   Serial.print(accel.x, 3);
+  Serial.print(",");
+  Serial.print(accel.y, 3);
+  Serial.print(",");
+  Serial.print(accel.z, 3);
+  Serial.println("");
+}
+
